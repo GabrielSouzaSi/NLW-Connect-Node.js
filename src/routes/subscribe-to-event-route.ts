2ac7c632d73641b843eafb7359959aeb08240575
GabrielSouzaSi <gos.gabri@gmail.com>
2025-02-20 11:01:21 -0400

    Conclusão NLW Connect Nodejs

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,5 +1,6 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
 import { z } from "zod"
+import { subscribeToEvent } from "../functions/subscribe-to-event"
 
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
   app.post(
@@ -11,21 +12,26 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
         body: z.object({
           name: z.string(),
           email: z.string().email(),
+          referrer: z.string().nullish(),
         }),
         response: {
           201: z.object({
-            name: z.string(),
-            email: z.string(),
+            subscriberId: z.string(),
           }),
         },
       },
     },
     async (request, replay) => {
-      const { name, email } = request.body
+      const { name, email, referrer } = request.body
 
-      return replay.status(200).send({
+      const { subscriberId } = await subscribeToEvent({
         name,
         email,
+        referrerId: referrer,
+      })
+
+      return replay.status(200).send({
+        subscriberId,
       })
     }
   )
